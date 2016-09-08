0268ce13e5f7bf68c75662559e93d5a80a9e707b
Nicolas Simond <nicolassimond@posteo.ch>
2016-09-08 19:03:02 +0200

    Update to kernel 4.7.3

--- kernel-install.sh
+++ kernel-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /tmp
-wget https://github.com/stylersnico/grsec-darkstar/releases/download/4.7.2/linux-image-4.7.2-grsec-darkstar-k_20160822_amd64.deb
-wget https://github.com/stylersnico/grsec-darkstar/releases/download/4.7.2/linux-image-4.7.2-grsec-darkstar-k_20160822_amd64.deb.sha256 
-sha256sum -c linux-image-4.7.2-grsec-darkstar-k_20160822_amd64.deb.sha256 
-dpkg -i linux-image-4.7.2-grsec-darkstar-k_20160822_amd64.deb
+wget https://github.com/stylersnico/grsec-darkstar/releases/download/4.7.3/linux-image-4.7.3-grsec-darkstar-k_20160908_amd64.deb
+wget https://github.com/stylersnico/grsec-darkstar/releases/download/4.7.3/linux-image-4.7.3-grsec-darkstar-k_20160908_amd64.deb.sha256 
+sha256sum -c linux-image-4.7.3-grsec-darkstar-k_20160908_amd64.deb.sha256 
+dpkg -i linux-image-4.7.3-grsec-darkstar-k_20160908_amd64.deb
