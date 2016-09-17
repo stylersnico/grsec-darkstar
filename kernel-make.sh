2905d7b28772d2f8e42bb2804cf25b69092c2d12
Nicolas Simond <nicolassimond@posteo.ch>
2016-09-17 10:48:43 +0200

    Update to Kernel 4.7.4

--- kernel-make.sh
+++ kernel-make.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 cd /tmp
-wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.7.3.tar.xz
-wget https://grsecurity.net/test/grsecurity-3.1-4.7.3-201609072139.patch
-tar -xvf linux-4.7.3.tar.xz
-cd linux-4.7.3
-patch -p1 < ../grsecurity-3.1-4.7.3-201609072139.patch
+wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.7.4.tar.xz
+wget https://grsecurity.net/test/grsecurity-3.1-4.7.4-201609152234.patch
+tar -xvf linux-4.7.4.tar.xz
+cd linux-4.7.4
+patch -p1 < ../grsecurity-3.1-4.7.4-201609152234.patch
 
 make menuconfig
 make-kpkg clean
-make-kpkg --jobs=16 --initrd --revision=20160908 kernel_image
+make-kpkg --jobs=16 --initrd --revision=20160917 kernel_image
