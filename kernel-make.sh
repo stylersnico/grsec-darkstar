a145dfdcdc1c3b29981e7adab90dbf0e5ca89b60
Nicolas Simond <nicolassimond@posteo.ch>
2016-10-02 09:49:59 +0200

    Update to Kernel 4.7.6

--- kernel-make.sh
+++ kernel-make.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 cd /tmp
-wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.7.4.tar.xz
-wget https://grsecurity.net/test/grsecurity-3.1-4.7.4-201609152234.patch
-tar -xvf linux-4.7.4.tar.xz
-cd linux-4.7.4
-patch -p1 < ../grsecurity-3.1-4.7.4-201609152234.patch
+wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.7.6.tar.xz
+wget https://grsecurity.net/test/grsecurity-3.1-4.7.6-201609301918.patch
+tar -xvf linux-4.7.6.tar.xz
+cd linux-4.7.6
+patch -p1 < ../grsecurity-3.1-4.7.6-201609301918.patch
 
 make menuconfig
 make-kpkg clean
-make-kpkg --jobs=16 --initrd --revision=20160917 kernel_image
+make-kpkg --jobs=16 --initrd --revision=20161002 kernel_image
