e3a5773904fea58801bea6316356baf9f9620977
Nicolas Simond <nicolassimond@posteo.ch>
2016-10-23 12:54:08 +0200

    Update to 4.7.10

--- kernel-make.sh
+++ kernel-make.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 cd /tmp
-wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.7.6.tar.xz
-wget https://grsecurity.net/test/grsecurity-3.1-4.7.6-201609301918.patch
-tar -xvf linux-4.7.6.tar.xz
-cd linux-4.7.6
-patch -p1 < ../grsecurity-3.1-4.7.6-201609301918.patch
+wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.7.10.tar.xz
+wget https://grsecurity.net/test/grsecurity-3.1-4.7.10-201610222037.patch
+tar -xvf linux-4.7.10.tar.xz
+cd linux-4.7.10
+patch -p1 < ../grsecurity-3.1-4.7.10-201610222037.patch
 
 make menuconfig
 make-kpkg clean
