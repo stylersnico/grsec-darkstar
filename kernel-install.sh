3a7987407023eada42d87d4b1a2f2eff8647e1e2
Nicolas Simond <nicolassimond@posteo.ch>
2016-10-23 15:48:43 +0200

    Update to 4.7.10

--- kernel-install.sh
+++ kernel-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /tmp
-wget https://github.com/stylersnico/grsec-darkstar/releases/download/4.7.4/linux-image-4.7.4-grsec-darkstar-k_20160917_amd64.deb
-wget https://github.com/stylersnico/grsec-darkstar/releases/download/4.7.4/linux-image-4.7.4-grsec-darkstar-k_20160917_amd64.deb.sha256 
-sha256sum -c linux-image-4.7.4-grsec-darkstar-k_20160917_amd64.deb.sha256 
-dpkg -i linux-image-4.7.4-grsec-darkstar-k_20160917_amd64.deb
+wget https://github.com/stylersnico/grsec-darkstar/releases/download/4.7.10/linux-image-4.7.10-grsec-darkstar-k_20161023_amd64.deb
+wget https://github.com/stylersnico/grsec-darkstar/releases/download/4.7.10/linux-image-4.7.10-grsec-darkstar-k_20161023_amd64.deb.sha256 
+sha256sum -c linux-image-4.7.10-grsec-darkstar-k_20161023_amd64.deb.sha256 
+dpkg -i linux-image-4.7.10-grsec-darkstar-k_20161023_amd64.deb
