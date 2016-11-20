bac482540e88939b8b24b13ed0c87d96467fcea2
Nicolas Simond <nicolassimond@posteo.ch>
2016-11-20 20:05:47 +0100

    Update to 4.8.9

--- kernel-make.sh
+++ kernel-make.sh
@@ -2,10 +2,10 @@
 
 cd /tmp
 wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.8.9.tar.xz
-wget https://grsecurity.net/test/grsecurity-3.1-4.8.7-201611102210.patch
+wget https://grsecurity.net/test/grsecurity-3.1-4.8.9-201611192033.patch
 tar -xvf linux-4.8.9.tar.xz
 cd linux-4.8.9
-patch -p1 < ../grsecurity-3.1-4.8.7-201611102210.patch
+patch -p1 < ../grsecurity-3.1-4.8.9-201611192033.patch
 
 make menuconfig
 make-kpkg clean
