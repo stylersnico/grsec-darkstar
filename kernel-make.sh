989decfe5a8779669780e10a9fdcba3cb1ce6821
Nicolas Simond <nicolassimond@posteo.ch>
2016-11-20 20:05:08 +0100

    Update kernel-make.sh

--- kernel-make.sh
+++ kernel-make.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 cd /tmp
-wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.8.7.tar.xz
+wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.8.9.tar.xz
 wget https://grsecurity.net/test/grsecurity-3.1-4.8.7-201611102210.patch
-tar -xvf linux-4.8.7.tar.xz
-cd linux-4.8.7
+tar -xvf linux-4.8.9.tar.xz
+cd linux-4.8.9
 patch -p1 < ../grsecurity-3.1-4.8.7-201611102210.patch
 
 make menuconfig
 make-kpkg clean
-make-kpkg --jobs=16 --initrd --revision=20161112 kernel_image
+make-kpkg --jobs=16 --initrd --revision=20161120 kernel_image
