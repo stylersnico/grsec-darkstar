e07c63080f499161eac0cf7a5176af509a958cf9
Nicolas Simond <nicolassimond@posteo.ch>
2017-02-12 15:13:37 +0100

    Update kernel-make.sh

--- kernel-make.sh
+++ kernel-make.sh
@@ -9,4 +9,4 @@ patch -p1 < ../grsecurity-3.1-4.9.8-201702071801.patch
 
 make menuconfig
 make-kpkg clean
-make-kpkg --jobs=2 --initrd --revision=20170208 kernel_image
+make-kpkg --jobs=16 --initrd --revision=20170212 kernel_image
