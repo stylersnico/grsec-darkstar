28744c8010489bd126110c887669235dd850e11b
Nicolas Simond <nicolassimond@posteo.ch>
2017-02-26 09:14:12 +0100

    Update to kernel 4.9.12

--- kernel-make.sh
+++ kernel-make.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 cd /tmp
-wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.9.8.tar.xz
-wget https://grsecurity.net/test/grsecurity-3.1-4.9.8-201702071801.patch
-tar -xvf linux-4.9.8.tar.xz
-cd linux-4.9.8
-patch -p1 < ../grsecurity-3.1-4.9.8-201702071801.patch
+wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.9.12.tar.xz
+wget https://grsecurity.net/test/grsecurity-3.1-4.9.12-201702231830.patch
+tar -xvf linux-4.9.12.tar.xz
+cd linux-4.9.12
+patch -p1 < ../grsecurity-3.1-4.9.12-201702231830.patch
 
 make menuconfig
 make-kpkg clean
-make-kpkg --jobs=16 --initrd --revision=20170212 kernel_image
+make-kpkg --jobs=16 --initrd --revision=20170226 kernel_image
