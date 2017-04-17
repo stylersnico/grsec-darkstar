de7d13f8c7dceea3e3c84209318c959f26ba815c
Nicolas Simond <nicolassimond@posteo.ch>
2017-04-17 13:50:49 +0200

    Update to 4.9.22

--- kernel-install.sh
+++ kernel-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /tmp
-wget https://github.com/stylersnico/grsec-darkstar/releases/download/4.9.8/linux-image-4.9.8-grsec-darkstar-k_20170212_amd64.deb
-wget https://github.com/stylersnico/grsec-darkstar/releases/download/4.9.8/linux-image-4.9.8-grsec-darkstar-k_20170212_amd64.deb.sha256
-sha256sum -c linux-image-4.9.8-grsec-darkstar-k_20170212_amd64.deb.sha256
-dpkg -i linux-image-4.9.8-grsec-darkstar-k_20170212_amd64.deb
+wget https://github.com/stylersnico/grsec-darkstar/releases/download/4.9.22/linux-image-4.9.22-grsec-darkstar-k_20170417_amd64.deb
+wget https://github.com/stylersnico/grsec-darkstar/releases/download/4.9.22/linux-image-4.9.22-grsec-darkstar-k_20170417_amd64.deb.sha256
+sha256sum -c linux-image-4.9.22-grsec-darkstar-k_20170417_amd64.deb.sha256
+dpkg -i linux-image-4.9.22-grsec-darkstar-k_20170417_amd64.deb
